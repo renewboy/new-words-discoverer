54dd551dd03fc9c90b772b7a4035b3f8bd04cfcb
zouyongxing <zouyongxing@sensetime.com>
2020-06-05 03:06:01 +0800

    build: refactor build using cmake

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # new-words-discoverer
-A new words discoverer without any existing knowledge base. Only support Chinese.
+## Introduction
+A new words discoverer without any existing knowledge base. Only Chinese is supported.
 
-Reference: http://www.matrix67.com/blog/archives/5044
+## Prerequisites
+- C++14 or later
+- [CMake](https://cmake.org/) 3.0 or later
+- [Boost](https://www.boost.org/)
+
+## Building
+```shell
+mkdir build
+cd build
+cmake [-DBOOST_ROOT=/path/to/your/boost/root/dir] [-DBOOST_LIBRARYDIR=/path/to/your/boost/lib] ..
+cmake --build . --target install --config Release
+cd ..
+```
+
+## Example
+You can simply run the example by following steps:
+- Build the application
+- Run the example: `cd install && ./new_words_discoverer ../corpus/红楼梦.txt`
+- Check the output in `红楼梦_out.txt`
+## Reference
+http://www.matrix67.com/blog/archives/5044
 
--- newwords_discoverer/Discoverer.cpp
+++ newwords_discoverer/Discoverer.cpp
@@ -1,4 +1,4 @@
-#include "Discoverer.h"
+ï»¿#include "Discoverer.h"
 #include <regex>
 #include <fstream>
 #include <iostream>
@@ -7,9 +7,10 @@
 #include <vector>
 #include <future>
 
-#include <boost/algorithm/string.hpp>     
+#include <boost/algorithm/string.hpp>
 
-namespace new_words_discover {
+namespace new_words_discover
+{
 
 void Discoverer::process()
 {
@@ -34,13 +35,11 @@ void Discoverer::process()
 
 void Discoverer::start_sentence_parser()
 {
-	sentence_parser_ = std::thread([this]
-	{
+	sentence_parser_ = std::thread([this] {
 		while (!file_parse_done_)
 		{
 			std::unique_lock<std::mutex> lck(mutex_sentence_);
-			cv_sentence_.wait(lck, [this]
-			{
+			cv_sentence_.wait(lck, [this] {
 				return !sentence_list_.empty() || file_parse_done_;
 			});
 
@@ -55,11 +54,10 @@ void Discoverer::start_sentence_parser()
 
 		while (!sentence_list_.empty())
 		{
-			const std::wstring& sentence = sentence_list_.front();
+			const std::wstring &sentence = sentence_list_.front();
 			parse_sentence(sentence);
 			sentence_list_.pop();
 		}
-
 	});
 }
 
@@ -74,9 +72,7 @@ int Discoverer::parse_file()
 	std::wstring paragraph;
 
 	// Warning C4129 Unrecognized character escape sequence.
-#pragma warning(disable:4129) 
 	std::wregex re(L"\W+|[a-zA-Z0-9]+|\s+|\n+");
-#pragma warning(default:4129)
 
 	std::cout << "calculating word frequency...\n";
 	while (std::getline(corpus, paragraph))
@@ -86,19 +82,14 @@ int Discoverer::parse_file()
 			continue;
 		}
 		std::vector<std::wstring> para_vec;
-		// Warning C4996 'std::copy::_Unchecked_iterators::_Deprecate': 
-		// Call to 'std::copy' with parameters that may be unsafe.
-#pragma warning(disable:4996) 
-		boost::algorithm::split(para_vec, paragraph, boost::is_any_of(L"¡¾¡¿£¬¡££¿¡¶¡·£¡¡¢£¨£©¡­¡­£»£º¡°¡±¡®¡¯"));
-#pragma warning(default:4996) 
-
-		for (auto& segment : para_vec)
+		boost::algorithm::split(para_vec, paragraph, boost::is_any_of(L"ã€ã€‘ï¼Œã€‚ï¼Ÿã€Šã€‹ï¼ã€ï¼ˆï¼‰â€¦â€¦ï¼›ï¼šâ€œâ€â€˜â€™"));
+		for (auto &segment : para_vec)
 		{
 			std::wsregex_token_iterator it(segment.begin(), segment.end(), re, -1);
 			std::wsregex_token_iterator end_it;
 			while (it != end_it)
 			{
-				std::wstring&& cur_sentence = it->str();
+				std::wstring &&cur_sentence = it->str();
 				++it;
 				if (!cur_sentence.empty())
 				{
@@ -120,7 +111,7 @@ int Discoverer::parse_file()
 	return 0;
 }
 
-void Discoverer::parse_sentence(const std::wstring & sentence)
+void Discoverer::parse_sentence(const std::wstring &sentence)
 {
 	assert(!sentence.empty());
 	for (size_t word_len = 1; word_len <= std::min(thresholds_.max_word_len, sentence.size()); ++word_len)
@@ -129,7 +120,7 @@ void Discoverer::parse_sentence(const std::wstring & sentence)
 	}
 }
 
-void Discoverer::parse_word(const std::wstring & sentence, size_t word_len)
+void Discoverer::parse_word(const std::wstring &sentence, size_t word_len)
 {
 	// Loop for the sentence to get all potential words.
 	for (size_t j = 0; j + word_len <= sentence.size(); ++j)
@@ -140,13 +131,13 @@ void Discoverer::parse_word(const std::wstring & sentence, size_t word_len)
 			if (j > 0)
 			{
 				// get the set of left adjacent Chinese character.
-				const wchar_t& left_adja = sentence[j - 1];
+				const wchar_t &left_adja = sentence[j - 1];
 				++std::get<1>(words_[word])[left_adja];
 			}
 			if (j + word_len < sentence.size())
 			{
-				// get the set of right adjacent Chinese character. 
-				const wchar_t& right_adja = sentence[j + word_len];
+				// get the set of right adjacent Chinese character.
+				const wchar_t &right_adja = sentence[j + word_len];
 				++std::get<2>(words_[word])[right_adja];
 			}
 		}
@@ -165,7 +156,7 @@ void Discoverer::remove_words_by_firmness()
 	}
 	auto low = words_.begin();
 	auto high = words_.begin();
-	auto step = total_words >= 4 ? total_words >> 2 : 1;      // Seperate into 4 or 5 parts.
+	auto step = total_words >= 4 ? total_words >> 2 : 1; // Seperate into 4 or 5 parts.
 	std::advance(high, step);
 
 	// high_int is to calculate the distance between high and words.end(),
@@ -175,8 +166,7 @@ void Discoverer::remove_words_by_firmness()
 	while (total_words - high_int >= 0)
 	{
 		// Go!
-		fu_vec.push_back(std::async(std::launch::async, [this, low, high]
-		{
+		fu_vec.push_back(std::async(std::launch::async, [this, low, high] {
 			calculate_firmness(low, high);
 		}));
 		if (high_int == total_words)
@@ -196,14 +186,13 @@ void Discoverer::remove_words_by_firmness()
 		}
 	}
 	// Waiting for the future objects.
-	for (auto& fu : fu_vec)
+	for (auto &fu : fu_vec)
 	{
 		fu.get();
 	}
 	for (auto it = words_.begin(); it != words_.end();)
 	{
-		if (words_.size() > 1
-			&& std::get<firmness_t>(it->second) < thresholds_.firmness_thr)
+		if (words_.size() > 1 && std::get<firmness_t>(it->second) < thresholds_.firmness_thr)
 		{
 			it = words_.erase(it);
 		}
@@ -214,15 +203,15 @@ void Discoverer::remove_words_by_firmness()
 	}
 }
 
-void Discoverer::calculate_firmness(std::pair<const std::wstring, word_t>& word)
+void Discoverer::calculate_firmness(std::pair<const std::wstring, word_t> &word)
 {
 	size_t p_word = std::get<frequency_t>(word.second);
 	double min_firmness = std::numeric_limits<double>::max();
-	
+
 	for (size_t i = 0; i < word.first.size(); ++i)
 	{
-		const auto& left_part = words_[word.first.substr(0, i + 1)];
-		const auto& right_part = words_[word.first.substr(i + 1, word.first.size() - i - 1)];
+		const auto &left_part = words_[word.first.substr(0, i + 1)];
+		const auto &right_part = words_[word.first.substr(i + 1, word.first.size() - i - 1)];
 		auto ans = static_cast<double>(p_word) * tot_frequency_ / (std::get<frequency_t>(left_part) * std::get<frequency_t>(right_part));
 		if (ans < min_firmness)
 		{
@@ -243,23 +232,21 @@ void Discoverer::calculate_firmness(word_map_iter begin, word_map_iter end)
 	}
 }
 
-double Discoverer::calculate_degree_of_freedom(const std::pair<std::wstring, word_t>& word)
+double Discoverer::calculate_degree_of_freedom(const std::pair<std::wstring, word_t> &word)
 {
-	const auto& left_adjas = std::get<1>(word.second);
-	const auto& right_adjas = std::get<2>(word.second);
+	const auto &left_adjas = std::get<1>(word.second);
+	const auto &right_adjas = std::get<2>(word.second);
 	return std::min(entropy(left_adjas), entropy(right_adjas));
 }
 
-double Discoverer::entropy(const std::unordered_map<wchar_t, frequency_t>& adjacents)
+double Discoverer::entropy(const std::unordered_map<wchar_t, frequency_t> &adjacents)
 {
 	size_t tot_freq = 0;
 	double ret = 0;
-	std::for_each(adjacents.begin(), adjacents.end(), [&tot_freq](auto& val)
-	{
+	std::for_each(adjacents.begin(), adjacents.end(), [&tot_freq](auto &val) {
 		tot_freq += val.second;
 	});
-	std::for_each(adjacents.begin(), adjacents.end(), [&tot_freq, &ret](auto& val)
-	{
+	std::for_each(adjacents.begin(), adjacents.end(), [&tot_freq, &ret](auto &val) {
 		ret += (-std::log2(static_cast<double>(val.second) / tot_freq) * (static_cast<double>(val.second) / tot_freq));
 	});
 	return ret;
@@ -299,12 +286,11 @@ void Discoverer::remove_words_by_degree_of_freedom()
 void Discoverer::print()
 {
 	std::vector<std::pair<std::wstring, word_t>> sorted(words_.begin(), words_.end());
-	std::sort(sorted.begin(), sorted.end(), [](auto& x, auto& y)
-	{
+	std::sort(sorted.begin(), sorted.end(), [](auto &x, auto &y) {
 		return std::get<frequency_t>(x.second) < std::get<frequency_t>(y.second);
 	});
 
-	// Generating output filename by input filename, remove suffix if any. 
+	// Generating output filename by input filename, remove suffix if any.
 	auto dot = filename_.find_last_of('.');
 	std::string out_file;
 	out_file = (dot != std::string::npos ? filename_.substr(0, dot) : filename_);
@@ -316,10 +302,10 @@ void Discoverer::print()
 		return;
 	}
 	output << L"Total words: " << sorted.size() << std::endl;
-	for (const auto& s : sorted)
+	for (const auto &s : sorted)
 	{
 		output << s.first << L" " << std::get<frequency_t>(s.second) << std::endl;
 	}
 	std::cout << "The results are stored in " + out_file << std::endl;
 }
-} //namespace
+} // namespace new_words_discover
